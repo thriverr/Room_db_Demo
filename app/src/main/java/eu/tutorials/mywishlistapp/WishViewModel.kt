d9d82969b1115c493253f2d8c3cf3ef3fadd5c86
thriverr <tushigupta44@gmail.com>
2024-05-24 11:59:55 +0530

    commiting

---
+++ app/src/main/java/eu/tutorials/mywishlistapp/WishViewModel.kt
@@ -0,0 +1,60 @@
+package eu.tutorials.mywishlistapp
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import eu.tutorials.mywishlistapp.data.Wish
+import eu.tutorials.mywishlistapp.data.WishRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class WishViewModel(
+    private val wishRepository: WishRepository = Graph.wishRepository
+):ViewModel() {
+
+    var wishTitleState by mutableStateOf("")
+    var wishDescriptionState by mutableStateOf("")
+
+
+    fun onWishTitleChanged(newString:String){
+        wishTitleState = newString
+    }
+
+    fun onWishDescriptionChanged(newString: String){
+        wishDescriptionState = newString
+    }
+
+    lateinit var getAllWishes: Flow<List<Wish>>
+
+    init {
+        viewModelScope.launch {
+            getAllWishes = wishRepository.getWishes()
+        }
+    }
+
+    fun addWish(wish: Wish){
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.addAWish(wish= wish)
+        }
+    }
+
+    fun getAWishById(id:Long):Flow<Wish> {
+        return wishRepository.getAWishById(id)
+    }
+
+    fun updateWish(wish: Wish){
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.updateAWish(wish= wish)
+        }
+    }
+
+    fun deleteWish(wish: Wish){
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.deleteAWish(wish= wish)
+            getAllWishes = wishRepository.getWishes()
+        }
+    }
+}
---
+++ app/src/main/java/eu/tutorials/mywishlistapp/data/Wish.kt
@@ -0,0 +1,28 @@
+package eu.tutorials.mywishlistapp.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="wish-table")
+data class Wish(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    @ColumnInfo(name="wish-title")
+    val title: String="",
+    @ColumnInfo(name="wish-desc")
+    val description:String=""
+)
+
+object DummyWish{
+    val wishList = listOf(
+        Wish(title="Google Watch 2",
+            description =  "An android Watch"),
+        Wish(title = "Oculus Quest 2",
+            description = "A VR headset for playing games"),
+        Wish(title = "A Sci-fi, Book",
+            description= "A science friction book from any best seller"),
+        Wish(title = "Bean bag",
+            description = "A comfy bean bag to substitute for a chair")
+        )
+}
